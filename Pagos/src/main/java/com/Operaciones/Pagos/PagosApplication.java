11e1793ba4d00a0123f38ab30cf39a82f27dc304
Jesus2002n <jesu.cardenas@duocuc.cl>
2026-05-18 21:57:28 -0400

    Microservicio pagos

---
+++ Pagos/src/main/java/com/Operaciones/Pagos/PagosApplication.java
@@ -0,0 +1,13 @@
+package com.Operaciones.Pagos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagosApplication.class, args);
+	}
+
+}
