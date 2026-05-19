a116643d65465139f1e18cce5600a4ca597952ee
Jesus2002n <jesu.cardenas@duocuc.cl>
2026-05-18 20:16:40 -0400

    Creacion carptea Clientes

---
+++ Clientes/src/test/java/com/gestion/gestionProject/GestionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.gestionProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
