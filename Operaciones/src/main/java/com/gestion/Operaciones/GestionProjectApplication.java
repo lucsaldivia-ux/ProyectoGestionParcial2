9fee9dfd564b8443e4907f2f5945988e4cb713a9
Jesus2002n <jesu.cardenas@duocuc.cl>
2026-05-18 20:12:17 -0400

    cambio de ruta

--- Operaciones/src/main/java/com/gestion/Operaciones/GestionProjectApplication.java
+++ Operaciones/src/main/java/com/gestion/Operaciones/GestionProjectApplication.java
@@ -1,4 +1,4 @@
-package com.gestion.gestionProject;
+package com.gestion.Operaciones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
